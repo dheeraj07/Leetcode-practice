4456669b8e45722ea2d5282f5aa539a08efd18e1
dheeraj07 <naluboludheerajreddy007@gmail.com>
2020-05-02 18:12:29 +0000

    Sync LeetCode submission - Jewels and Stones (cpp)

--- problems/jewels_and_stones/solution.cpp
+++ problems/jewels_and_stones/solution.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     int numJewelsInStones(string J, string S) {
-        vector<int> v(123,0);
-        for(int i=0;i<J.length();i++){
-            v[J[i]]++;
+        set<char> jewels;
+        int ans = 0;
+        for(int i=0;i<J.length();i++) jewels.insert(J[i]);
+        
+        for(int i=0;i<S.length();i++)
+        {
+            if(jewels.find(S[i])!=jewels.end())
+            {
+                ans++;
+            }
         }
-        int count = 0;
-        for(int j=0;j<S.length();j++){
-            if(v[S[j]]>0) count++;
-        }
-        return count;       
+        return ans; 
     }
 };
