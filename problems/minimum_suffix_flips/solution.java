e4457561b548db02c17dae244703528d447c0530
dheeraj07 <naluboludheerajreddy007@gmail.com>
2022-11-27 04:04:47 +0000

    Sync LeetCode submission - Minimum Suffix Flips (java)

---
+++ problems/minimum_suffix_flips/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    
+    public int minFlips(String target) {
+        char prev = '0';
+        int res = 0;
+        for (char c : target.toCharArray()) {
+            if (c != prev) {
+                prev = c;
+                res++;
+            }
+        }
+        return res; 
+    }
+}
