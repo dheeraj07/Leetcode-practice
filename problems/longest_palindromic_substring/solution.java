b7c7b225ad3eb56adcda0de0708157f3a15fc675
dheeraj07 <naluboludheerajreddy007@gmail.com>
2022-12-27 19:33:49 +0000

    Sync LeetCode submission - Longest Palindromic Substring (java)

--- problems/longest_palindromic_substring/solution.java
+++ problems/longest_palindromic_substring/solution.java
@@ -1,33 +1,43 @@
 class Solution {
     public String longestPalindrome(String s) {
-        if (s == null || s.length() < 1) return "";
-    int start = 0, end = 0;
-    for (int i = 0; i < s.length(); i++) {
-        int len1 = expandAroundCenter(s, i, i); 
-        //expands around center to check for palindrome of odd length
-        int len2 = expandAroundCenter(s, i, i + 1);
-        //expands around center to check for palindrome of even length
-        int len = Math.max(len1, len2); 
-        //Compares the two lengths and assigns the larger value to len
-        if (len > end - start) {
-            start = i - (len - 1) / 2;
-            //Assigns the start index of the longest palindrome
-            end = i + len / 2;
-            //Assigns the end index of the longest palindrome
+        String ans = ""+s.charAt(0);
+        int index;
+        if(s.length() == 1)
+        {
+            return s;
         }
-    }
-    return s.substring(start, end + 1);
+        
+        for(int i = 0; i < s.length(); i++)
+        {
+            index = ans.length();
+            for(int j = i+index; j < s.length(); j++)
+            {
+                if(checkIsPalindrome(i,j,ans, s))
+                {
+                    ans = s.substring(i, j+1);
+                    System.out.println(ans);
+                }
+            }
+        }
+        return ans;
     }
     
-    private int expandAroundCenter(String s, int left, int right) {
-    int L = left, R = right; //Assigns the left and right indices 
-    while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
-        //Compares the characters at the left and right indices, and if they're the same, 
-        //the indices are decremented and incremented, respectively
-        L--;
-        R++;
+    public boolean checkIsPalindrome(int i, int j, String ans, String s)
+    {
+        if(j - i + 1 <= ans.length())
+        {
+           return false; 
+        }
+        
+        while(i < j)
+        {
+            if(s.charAt(i++) != s.charAt(j--))
+            {
+               return false; 
+            }
+        }
+        return true;
     }
-    return R - L - 1; 
-    //returns the length of the palindrome
-}
+
+      
 }
